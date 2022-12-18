4aa4a4b1ff352334c81119862af57e3b1eae1942
Amir Fazliev <fazliev.amir@yandex.ru>
2022-12-18 13:58:15 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class PhoneBook {
+
+    public boolean add() {
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,18 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest () {
+        PhoneBook phoneBook = new PhoneBook();
+
+        int expected = 1;
+
+        int result = phoneBook.add();
+
+        assertEquals (expected, result);
+    }
 }
