a90f9c6f09dd5dc6a12b73ca5354ba2f373238b3
Amir Fazliev <fazliev.amir@yandex.ru>
2022-12-18 16:26:41 +0300

    Refactor code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("29926574", "Petya");
+        phoneBook.add("54864161", "Qwerty");
+        phoneBook.add("767218", "Amir");
+        phoneBook.printAllNames();
+
     }
 }
--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -7,26 +7,40 @@ public class PhoneBook {
     private HashMap<String, String> hashMap = new HashMap<>();
 
     public boolean add(String phoneNumber, String name) {
-        if (hashMap.isEmpty() || !hashMap.get(phoneNumber).equals(name)) {
+        try {
+            if (hashMap.get(phoneNumber).equals(name)) {
+                System.out.println("Ошибка: попытка добавления одинакового контакта. Попробуйте еще раз");
+                return false;
+            }
+        } catch (NullPointerException e){
             hashMap.put(phoneNumber, name);
             System.out.println("Количество контактов в телефонной книге - " + hashMap.size());
             return true;
         }
-        System.out.println("Ошибка: попытка добавления одинакового контакта. Попробуйте еще раз");
         return false;
     }
 
     public String findByNumber(String phoneNumber) {
-        return hashMap.get(phoneNumber);
+        try {
+            return hashMap.get(phoneNumber);
+        } catch (NullPointerException e) {
+            System.out.println("Пустая телефонная книжка");
+            return null;
+        }
     }
 
     public String findByName (String name) {
-        for (String key : hashMap.keySet()) {
-            if(hashMap.get(key).equals(name)){
-                return key;
+        try {
+            for (String key : hashMap.keySet()) {
+                if (hashMap.get(key).equals(name)) {
+                    return key;
+                }
             }
+            return null;
+        } catch (NullPointerException e) {
+            System.out.println("Пустая телефонная книжка");
+            return null;
         }
-        return null;
     }
 
     public void printAllNames() {
